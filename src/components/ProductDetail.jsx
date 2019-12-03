4698742f59f76444d47e127ff33719484d7b9131
Nur <siti_sli@hotmail.com>
2019-12-03 15:39:40 +0700

    add security login

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios'
 import queryString from 'query-string'
 import {API_URL} from '../supports/api_url'
